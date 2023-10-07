d6ca60921a16d1aeecde9cafa753643b29ac0624
Valentín Galfré <galfre.vn@gmail.com>
2023-10-07 02:46:06 -0300

    ci: updating continuos integration configuration to deploy to @vercel

--- apps/site/next.config.mjs
+++ apps/site/next.config.mjs
@@ -1,15 +1,6 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfiguration = {
   transpilePackages: ['@conexa/environment'],
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.API_URL}/:path*`,
-      },
-    ];
-  },
 };
 
-export default nextConfiguration
+export default nextConfiguration;
