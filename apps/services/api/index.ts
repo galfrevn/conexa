ce460959720ee6a860a8884c9a66bf15a7b5641b
Valentin Galfre <galfre.vn@gmail>
2023-10-10 13:54:52 -0300

    build: installing netlify serverless deploy packages

--- apps/services/api/index.ts
+++
@@ -1,3 +0,0 @@
-import * as app from '../src/app';
-
-export default app;
--- apps/services/src/app.ts
+++ apps/services/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import serverless from 'serverless-http';
 
 import { setupServer } from '@/lib/setup';
 import { serverConfiguration } from '@/configuration/server';
@@ -7,12 +8,15 @@ export const initializeServer = () => {
   const application: Application = express();
   setupServer(application);
 
+  // Export application before listening to avoid EADDRINUSE error in tests
   if (process.env.NODE_ENV === 'test') return application;
 
   application.listen(serverConfiguration.port, () => {
     const { environment, port } = serverConfiguration;
     console.log(`[${environment}] 🚀 Server is running on port ${port}!`);
   });
+
+  return application;
 };
 
-initializeServer();
+serverless(initializeServer());
